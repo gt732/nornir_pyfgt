eec75ce94848be4a10387143c01dd578d06fa144
GT <geuryt@yahoo.com>
2023-02-05 10:49:34 -0500

    create setup.py file

--- nornir_fgt/plugins/tasks/pyfgt_send_command.py
+++ nornir_fgt/plugins/tasks/pyfgt_send_command.py
@@ -1,3 +1,13 @@
 from typing import Any, Dict, List, Optional
 from nornir.core.task import Result, Task
-from nornir.plugins.connections import CONNECTION_NAME
+from nornir_pyfgt.plugins.connections import CONNECTION_NAME
+
+
+def pyfgt_send_command(
+    task: Task,
+) -> Result:
+
+    device = task.host.get_connection(CONNECTION_NAME, task.nornir.config)
+
+    result = device.facts
+    return Result(host=task.host, result=result)
