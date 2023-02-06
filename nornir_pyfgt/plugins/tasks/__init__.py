95328c9c078328dd0682f7f8db2ee06dbd7bd1d9
GT <geuryt@yahoo.com>
2023-02-06 11:39:02 -0500

    added dhcp task

--- nornir_pyfgt/plugins/tasks/__init__.py
+++ nornir_pyfgt/plugins/tasks/__init__.py
@@ -3,6 +3,9 @@ from .pyfgt_address import pyfgt_address
 from .pyfgt_address_grp import pyfgt_address_grp
 from .pyfgt_antivirus import pyfgt_antivirus
 from .pyfgt_application import pyfgt_application
+from .pyfgt_dhcp_server import pyfgt_dhcp_server
+
+
 
 
 __all__ = (
@@ -11,5 +14,6 @@ __all__ = (
     "pyfgt_address_grp",
     "pyfgt_antivirus",
     "pyfgt_application",
+    "pyfgt_dhcp_server",
 
 )
