f4be873945bad9431f112bb704105d2743a5fdec
GT <geuryt@yahoo.com>
2023-02-06 11:56:35 -0500

    created interface task

--- nornir_pyfgt/plugins/tasks/__init__.py
+++ nornir_pyfgt/plugins/tasks/__init__.py
@@ -4,7 +4,17 @@ from .pyfgt_address_grp import pyfgt_address_grp
 from .pyfgt_antivirus import pyfgt_antivirus
 from .pyfgt_application import pyfgt_application
 from .pyfgt_dhcp_server import pyfgt_dhcp_server
-
+from .pyfgt_interface import pyfgt_interface
+from .pyfgt_internet_service import pyfgt_internet_service
+# from .pyfgt_ip_pool import pyfgt_ip_pool
+# from .pyfgt_policy import pyfgt_policy
+# from .pyfgt_schedule import pyfgt_schedule
+# from .pyfgt_service import pyfgt_service
+# from .pyfgt_service_category import pyfgt_service_category
+# from .pyfgt_service_group import pyfgt_service_group
+# from .pyfgt_snmp_community import pyfgt_snmp_community
+# from .pyfgt_virtual_ip import pyfgt_virtual_ip
+# from .pyfgt_zone import pyfgt_zone
 
 
 
@@ -15,5 +25,16 @@ __all__ = (
     "pyfgt_antivirus",
     "pyfgt_application",
     "pyfgt_dhcp_server",
+    "pyfgt_interface",
+    "pyfgt_internet_service",
+    "pyfgt_ip_pool",
+    "pyfgt_policy",
+    "pyfgt_schedule",
+    "pyfgt_service",
+    "pyfgt_service_category",
+    "pyfgt_service_group",
+    "pyfgt_snmp_community",
+    "pyfgt_virtual_ip",
+    "pyfgt_zone",
 
 )
