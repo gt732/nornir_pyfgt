917a408846cc163b0dca65112d5439c3f624df1a
GT <geuryt@yahoo.com>
2023-02-06 11:03:27 -0500

    added get fw address task

--- nornir_pyfgt/plugins/tasks/__init__.py
+++ nornir_pyfgt/plugins/tasks/__init__.py
@@ -1,6 +1,8 @@
 from .pyfgt_send_command import pyfgt_send_command
+from .pyfgt_get_fw_address import pyfgt_get_fw_address
 
 __all__ = (
     "pyfgt_send_command",
+    "pyfgt_get_fw_address",
 
 )
