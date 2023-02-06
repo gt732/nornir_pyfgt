b4c33112bd3bba4cb01a573503e1a17989676b60
GT <geuryt@yahoo.com>
2023-02-06 11:35:31 -0500

    added application task

--- nornir_pyfgt/plugins/tasks/__init__.py
+++ nornir_pyfgt/plugins/tasks/__init__.py
@@ -2,7 +2,7 @@ from .pyfgt_send_command import pyfgt_send_command
 from .pyfgt_address import pyfgt_address
 from .pyfgt_address_grp import pyfgt_address_grp
 from .pyfgt_antivirus import pyfgt_antivirus
-
+from .pyfgt_application import pyfgt_application
 
 
 __all__ = (
@@ -10,5 +10,6 @@ __all__ = (
     "pyfgt_address",
     "pyfgt_address_grp",
     "pyfgt_antivirus",
+    "pyfgt_application",
 
 )
